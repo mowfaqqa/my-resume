fbe81610ed5fb8ac26dfc740aeba0f4b6a955995
Muwaffaq <itzmuwaffaq@gmail.com>
2021-08-18 10:15:52 +0100

    first commit

---
+++ js(Question-2)/main.js
@@ -0,0 +1,30 @@
+// Queustion 2
+const name = 'Muwaffaq Aliyu'
+const nameInput = document.getElementById('name')
+
+console.log(`My name is ${name}`)
+nameInput.innerHTML = `<h2>${name}</h2>`;
+
+
+
+
+// skills card
+const clc = document.querySelector(".cancel")
+const arr = document.querySelector(".arr_container")
+const leftContainer = document.querySelector(".left-container")
+
+arr.addEventListener("click", ()=> {
+   arr.classList.add("active_arr");
+   if(leftContainer.classList.contains("off")){
+      leftContainer.classList.remove("off")
+      leftContainer.classList.add("active")
+   }
+})
+
+clc.addEventListener("click", ()=>{
+   arr.classList.remove("active_arr");
+   if(leftContainer.classList.contains("active")){
+      leftContainer.classList.remove("active");
+      leftContainer.classList.add("off")
+   }
+})
